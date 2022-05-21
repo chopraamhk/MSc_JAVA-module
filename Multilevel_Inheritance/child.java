f38bd3f4255cd5cb7eecfc7be628994df7384697
Mehak Chopra <mehakchopra@iisc.ac.in>
2022-05-21 16:23:11 +0530

    Update child.java

--- Multilevel_Inheritance/child.java
+++ Multilevel_Inheritance/child.java
@@ -1,25 +1,25 @@
-//child class1 inheritance (evolution of horse)
-public class child1 extends child
+//child class inheritance (evolution of horse)
+public class child extends parent
  {
    String hname;
    String stage;
    double length;
    double period;
   
-   void getchild1(String h, String s, double l, double p)
+   void getchild(String h, String s, double l, double p)
    {
-    hname=h;
-    stage=s;
-    length=l;
-    period=p;
+     hname=h;
+     stage=s;
+     length=l;
+     period=p;
    }
    
-   void display()
+    void display()
    {
     super.display();
     System.out.println("Next horse was " +hname);
     System.out.println("stage of the horse was " +stage);
     System.out.println("length of the horse was " +length);
-    System.out.println("Period was (in-million-years-ago) " +period);
+    System.out.println("period was " +period);
    }
- }
+   }
